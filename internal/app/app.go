cb37afdc10061240443d4cddf186cdbec8fa6fb0
midoks <midoks@163.com>
2022-02-03 21:39:33 +0800

    up

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,17 +4,19 @@ import (
 	"fmt"
 	"net/http"
 	"net/rpc"
-	// "os"
-	// "path/filepath"
+
+	"github.com/go-facegit/facegit-rpc/internal/app/lib"
 )
 
 func Start(port int) {
 
 	sPortStr := fmt.Sprintf(":%d", port)
 
-	fmt.Println(sPortStr)
-
 	rpc.HandleHTTP()
+
+	repoLib := new(lib.Repo)
+
+	rpc.Register(repoLib)
 	err := http.ListenAndServe(sPortStr, nil)
 	if err != nil {
 		fmt.Println("rpc listen err:%s", err.Error())
--- internal/app/lib/repo.go
+++ internal/app/lib/repo.go
@@ -1,1 +1,19 @@
 package lib
+
+import (
+	"fmt"
+)
+
+type Repo struct {
+	s string
+}
+
+type RepoCreateArgs struct {
+	A, B int
+}
+
+func (rp *Repo) Create(args *RepoCreateArgs, reply *int) error {
+	fmt.Println("这个方法执行了啊---嘿嘿--- Multiply ")
+	*reply = 1
+	return nil
+}
