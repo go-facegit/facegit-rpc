e8f5578726bdf64325d999644495dbec5affbbfb
midoks <midoks@163.com>
2022-02-10 00:17:34 +0800

    Update repo.go

--- internal/app/lib/repo.go
+++ internal/app/lib/repo.go
@@ -2,6 +2,8 @@ package lib
 
 import (
 	"fmt"
+	"os"
+	// "time"
 
 	"github.com/go-facegit/facegit-rpc/internal/conf"
 	"github.com/go-facegit/facegit-rpc/internal/tools"
@@ -9,25 +11,35 @@ import (
 )
 
 type Repo struct {
-	s string
 }
 
 type RepoCreateArgs struct {
-	UserName    string
+	UserOrOrg   string
 	ProjectName string
 }
 
+//Init Git Repo Project
 func (rp *Repo) Create(args *RepoCreateArgs, reply *bool) error {
-
+	*reply = false
 	rootPath := conf.Repo.RootPath
-	if tools.IsExist(rootPath) {
-
-		repoPath := fmt.Sprintf("%s/%s/%s.git", rootPath, args.UserName, args.ProjectName)
+	repoPath := fmt.Sprintf("%s/%s/%s.git", rootPath, args.UserOrOrg, args.ProjectName)
+	if !tools.IsExist(repoPath) {
 		if err := git.Init(repoPath, git.InitOptions{Bare: true}); err != nil {
 			return fmt.Errorf("init repository: %v", err)
 		}
+		*reply = true
 	}
+	return nil
+}
 
+func (rp *Repo) Delete(args *RepoCreateArgs, reply *bool) error {
+	*reply = false
+	rootPath := conf.Repo.RootPath
+	repoPath := fmt.Sprintf("%s/%s/%s.git", rootPath, args.UserOrOrg, args.ProjectName)
+	if err := os.RemoveAll(repoPath); err != nil {
+		desc := fmt.Sprintf("[%s]: %v", repoPath, err)
+		return fmt.Errorf("delete repository: %s", desc)
+	}
 	*reply = true
 	return nil
 }
