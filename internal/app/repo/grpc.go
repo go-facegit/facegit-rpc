1cd597f69296b59efc01d23ea82995a79f0695a4
midoks <midoks@163.com>
2022-03-21 23:44:09 +0800

    up

--- internal/app/repo/grpc.go
+++ internal/app/repo/grpc.go
@@ -21,9 +21,32 @@ func (s *Server) Create(ctx context.Context, message *pb.ReqBase) (*pb.RespBool,
 	return &pb.RespBool{TrueOrFalse: tof}, err
 }
 
-func (s *Server) List(ctx context.Context, message *pb.ReqList) (*pb.Message, error) {
+func (s *Server) List(ctx context.Context, message *pb.ReqList) (*pb.RespList, error) {
+	err, data := RepoList(message.UserOrOrg, message.ProjectName, message.TreePath)
 
-	b, err, ee := RepoList(message.UserOrOrg, message.ProjectName, message.TreePath)
-	fmt.Println(b, err, ee)
-	return &pb.Message{Body: "hh"}, nil
+	if err != nil {
+		return &pb.RespList{}, err
+	}
+
+	list := data.List
+	var retList []*pb.RespStructList
+	for _, v := range list {
+		// fmt.Println("dlist:", k, v.Entry.Name(), v.Commit.ID, v.Entry.Type())
+		t := &pb.RespStructList{
+			Name:     v.Entry.Name(),
+			CommitId: fmt.Sprintf("%s", v.Commit.ID),
+			Type:     fmt.Sprintf("%s", v.Entry.Type()),
+		}
+
+		retList = append(retList, t)
+	}
+
+	return &pb.RespList{
+		Newest: &pb.RespStructNewest{
+			Message:    data.Newest.Message,
+			Id:         data.Newest.Id,
+			AuthorName: data.Newest.AuthorName,
+		},
+		List: retList,
+	}, nil
 }
